8260b7f833eef5dc3740d9c5bad67e7e17b9206c
unknown <saks0705@gmail.com>
2024-03-14 22:04:38 +0530

    files updated

--- Js/index.js
+++ Js/index.js
@@ -11,49 +11,47 @@ const playlists = [
 
 // Function to create a card element
 function createCard(playlist) {
-    const {image,title,description}=playlist
-    const card = document.createElement('div');
-    card.classList.add('col-lg-3');
-    card.classList.add('col-md-6');
-    card.classList.add('col-sm-6');
+  const {image, title, description} = playlist;
+  const card = document.createElement('div');
+  card.classList.add('col-lg-3');
+  card.classList.add('col-md-6');
+  card.classList.add('col-sm-6');
 
-    card.innerHTML = `
-      <div class="card">
-        <img src="${image}" class="card-img-top" alt="${title}">
-        <div class="card-body">
-          <h5 class="card-title">${title}</h5>
-          <p class="card-text">${description}</p>
-        </div>
+  card.innerHTML = `
+    <div class="card">
+      <img src="${image}" class="card-img-top" alt="${title}">
+      <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text">${description}</p>
       </div>
-    `;
+    </div>
+  `;
 
-    return card;
+  return card;
 }
 
 // Function to append cards to the container
 function appendCards() {
-
-    const container = document.getElementById('playlist-row');
-    container.innerHTML = '';
-    container.style.maxHeight = '43vh';
-    playlists.slice(0, 4).forEach(playlist => {
-        const card = createCard(playlist);
-        container.appendChild(card);
-    });
+  const container = document.getElementById('playlist-row');
+  container.innerHTML = '';
+  container.style.maxHeight = '43vh';
+  playlists.slice(0, 4).forEach(playlist => {
+      const card = createCard(playlist);
+      container.appendChild(card);
+  });
 }
-
 // Function to show all cards
 function showAllCards() {
-    const container = document.getElementById('playlist-row');
-    container.innerHTML = '';
-    container.style.maxHeight = '43vh';
-    container.style.overflowY = 'auto';
-    container.style.scrollbarWidth = '.7';
-    container.style.scrollbarColor = 'rgb(255, 58, 153) var(--color-secondary)';
-    playlists.forEach(playlist => {
-        const card = createCard(playlist);
-        container.appendChild(card);
-    });
+  const container = document.getElementById('playlist-row');
+  container.innerHTML = '';
+  container.style.maxHeight = '43vh';
+  container.style.overflowY = 'auto';
+  container.style.scrollbarWidth = '.7';
+  container.style.scrollbarColor = 'rgb(255, 58, 153) var(--color-secondary)';
+  playlists.forEach(playlist => {
+      const card = createCard(playlist);
+      container.appendChild(card);
+  });
 }
 
 window.onload = appendCards;
